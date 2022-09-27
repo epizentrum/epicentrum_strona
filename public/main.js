aaa1ff8ba6bfb6af1791e837d58c6762d3d04d3b
flashdens <97975794+flashdens@users.noreply.github.com>
2022-09-27 21:14:02 +0200

    Revert "githubie dzialaj"

--- public/main.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('load', e => {
-	setTimeout(() => {
-		document.querySelector('#loading-screen').style.opacity = '0';
-	}, 3000);
-	setTimeout(() => {
-		document.querySelector('#loading-screen').style.display = 'none';
-	}, 3500);
-});
